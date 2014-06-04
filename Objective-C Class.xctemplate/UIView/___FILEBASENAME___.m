28fa849c72fd8b296db4b86efea798a2afa2d641
Doug Scandrett <doug@dssdigital.com>
2014-06-04 13:19:49 -0400

    Adjust a few minor formatting issues.

--- Objective-C Class.xctemplate/UIView/___FILEBASENAME___.m
+++ Objective-C Class.xctemplate/UIView/___FILEBASENAME___.m
@@ -13,53 +13,28 @@
 
 @implementation ___FILEBASENAMEASIDENTIFIER___
 
-- (id)initWithFrame: (CGRect)frame
-{
-	// Abort if base initializer fails.
-	if ((self = [super initWithFrame: frame]) == nil)
-	{
+- (id)initWithFrame: (CGRect)frame {
+	if ((self = [super initWithFrame: frame]) == nil) {
 		return nil;
 	}
-	
-	// Initialize view.
-	[self _initialize___VARIABLE_className:identifier___];
-	
-	// Return initialized instance.
-	return self;
+    [self _initialize___VARIABLE_className:identifier___];
+    return self;
 }
 
-- (id)initWithCoder: (NSCoder *)coder
-{
-	// Abort if base initializer fails.
-	if ((self = [super initWithCoder: coder]) == nil)
-	{
+- (id)initWithCoder: (NSCoder *)coder {
+	if ((self = [super initWithCoder: coder]) == nil) {
 		return nil;
 	}
-	
-	// Initialize view.
-	[self _initialize___VARIABLE_className:identifier___];
-	
-	// Return initialized instance.
-	return self;
+    [self _initialize___VARIABLE_className:identifier___];
+    return self;
 }
 
-
-#pragma mark - Public Methods
-
-
-#pragma mark - Overridden Methods
-
-
-#pragma mark - Private Methods
-
-- (void)_initialize___VARIABLE_className:identifier___
-{
+- (void)_initialize___VARIABLE_className:identifier___ {
 	// Initialize instance variables.
 }
 
 #pragma mark - Factory Methods
-+ (instancetype)standard___FILEBASENAMEASIDENTIFIER___
-{
++ (instancetype)standard___FILEBASENAMEASIDENTIFIER___ {
     ___FILEBASENAMEASIDENTIFIER___ *view = (___FILEBASENAMEASIDENTIFIER___ *)[[[NSBundle mainBundle] loadNibNamed:@"___FILEBASENAMEASIDENTIFIER___" owner:nil options:nil] objectAtIndex:0];
     return view;
 }
